72092bd9aee74e7f0d707d4c1f8fcb0b38ed2165
lanmower <almagestfraternite@gmail.com>
2019-03-06 09:18:07 +0200

    Update dconnect-reward.cpp

--- dconnect-reward.cpp
+++ dconnect-reward.cpp
@@ -1,71 +1,170 @@
-#include <eosiolib/eosio.hpp>
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
 
-using namespace eosio;
+#include <eosio.token/eosio.token.hpp>
 
-CONTRACT dconnect_reward: public contract {
-  public:
-      using contract::contract;
-      dconnect_reward( name receiver, name code, datastream<const char*> ds )
-         : contract(receiver, code, ds)
+namespace eosio {
 
-      ACTION claim(name account) {
-         require_auth(account);
-    	   
-         auto time = now();
-         post_tables postusertab(_self, account.value);
+void token::create( name   issuer,
+                    asset  maximum_supply )
+{
+    require_auth( _self );
 
-         posttab.emplace( _self, [&]( auto& u ) {
-             u.primary = pk;
-             u.key = key;
-             u.value = value;
-             u.owner = account;
-             u.app = app;
-      	     u.created = now();
-         });   
-      }
+    auto sym = maximum_supply.symbol;
+    eosio_assert( sym.is_valid(), "invalid symbol name" );
+    eosio_assert( maximum_supply.is_valid(), "invalid supply");
+    eosio_assert( maximum_supply.amount > 0, "max-supply must be positive");
 
-      ACTION reward(name account, uint64_t primary, std::string value) {
-         require_auth(account);
-         auto itr = posttab.find(primary);
-         eosio_assert(itr != posttab.end(), "not found");
-         eosio_assert(itr->owner.value == account.value, "only the owner is allowed to modify this content");
-         
-         posttab.modify( itr, _self, [&]( auto& row ) {
-           row.value = value;
-           row.modified = now();
-         });
-         post_tables postusertab(_self, account.value);
-         postusertab.modify( postusertab.find(primary), _self, [&]( auto& row ) {
-           row.value = value;
-           row.modified = now();
-         });
-         post_tables postapptab(_self, app.value);
-         postapptab.modify( postapptab.find(primary), _self, [&]( auto& row ) {
-           row.value = value;
-           row.modified = now();
-         });
-      }    
+    stats statstable( _self, sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    eosio_assert( existing == statstable.end(), "token with symbol already exists" );
 
-      TABLE post_table {
-         uint64_t primary;
-         name key;
-         name owner;
-         std::string value;
-         name app;
-      	 uint32_t created;
-      	 uint32_t modified;
-         uint64_t primary_key() const { return primary; }
-         uint64_t by_key() const { return key.value; }
-         uint64_t by_owner() const { return owner.value; }
-         uint64_t by_app() const { return app.value; }
-      };
+    statstable.emplace( _self, [&]( auto& s ) {
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
+    });
+}
 
-      typedef eosio::multi_index<"posts"_n, post_table,  eosio::indexed_by<"key"_n, eosio::const_mem_fun<post_table, uint64_t, &post_table::by_key>>,  eosio::indexed_by<"ownerkey"_n, eosio::const_mem_fun<post_table, uint64_t, &post_table::by_owner>>,  eosio::indexed_by<"appkey"_n, eosio::const_mem_fun<post_table, uint64_t, &post_table::by_app>>> post_tables;
 
-      using claim_action     = action_wrapper<  "set"_n, &dconnect_reward::claim>;
-      using reward_action    = action_wrapper< "edit"_n, &dconnect_reward::reward>;
-      private:
-        post_tables posttab;
-};
+void token::issue( name to, asset quantity, string memo )
+{
+    auto sym = quantity.symbol;
+    eosio_assert( sym.is_valid(), "invalid symbol name" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
 
-EOSIO_DISPATCH( dconnect_reward, (claim)(reward) )
+    stats statstable( _self, sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
+    const auto& st = *existing;
+
+    require_auth( st.issuer );
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must issue positive quantity" );
+
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply += quantity;
+    });
+
+    add_balance( st.issuer, quantity, st.issuer );
+
+    if( to != st.issuer ) {
+      SEND_INLINE_ACTION( *this, transfer, { {st.issuer, "active"_n} },
+                          { st.issuer, to, quantity, memo }
+      );
+    }
+}
+
+void token::retire( asset quantity, string memo )
+{
+    auto sym = quantity.symbol;
+    eosio_assert( sym.is_valid(), "invalid symbol name" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    stats statstable( _self, sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    eosio_assert( existing != statstable.end(), "token with symbol does not exist" );
+    const auto& st = *existing;
+
+    require_auth( st.issuer );
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must retire positive quantity" );
+
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply -= quantity;
+    });
+
+    sub_balance( st.issuer, quantity );
+}
+
+void token::transfer( name    from,
+                      name    to,
+                      asset   quantity,
+                      string  memo )
+{
+    eosio_assert( from != to, "cannot transfer to self" );
+    require_auth( from );
+    eosio_assert( is_account( to ), "to account does not exist");
+    auto sym = quantity.symbol.code();
+    stats statstable( _self, sym.raw() );
+    const auto& st = statstable.get( sym.raw() );
+
+    require_recipient( from );
+    require_recipient( to );
+
+    eosio_assert( quantity.is_valid(), "invalid quantity" );
+    eosio_assert( quantity.amount > 0, "must transfer positive quantity" );
+    eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    auto payer = has_auth( to ) ? to : from;
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, payer );
+}
+
+void token::sub_balance( name owner, asset value ) {
+   accounts from_acnts( _self, owner.value );
+
+   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+   eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
+
+   from_acnts.modify( from, owner, [&]( auto& a ) {
+         a.balance -= value;
+      });
+}
+
+void token::add_balance( name owner, asset value, name ram_payer )
+{
+   accounts to_acnts( _self, owner.value );
+   auto to = to_acnts.find( value.symbol.code().raw() );
+   if( to == to_acnts.end() ) {
+      to_acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = value;
+      });
+   } else {
+      to_acnts.modify( to, same_payer, [&]( auto& a ) {
+        a.balance += value;
+      });
+   }
+}
+
+void token::open( name owner, const symbol& symbol, name ram_payer )
+{
+   require_auth( ram_payer );
+
+   auto sym_code_raw = symbol.code().raw();
+
+   stats statstable( _self, sym_code_raw );
+   const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+   eosio_assert( st.supply.symbol == symbol, "symbol precision mismatch" );
+
+   accounts acnts( _self, owner.value );
+   auto it = acnts.find( sym_code_raw );
+   if( it == acnts.end() ) {
+      acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = asset{0, symbol};
+      });
+   }
+}
+
+void token::close( name owner, const symbol& symbol )
+{
+   require_auth( owner );
+   accounts acnts( _self, owner.value );
+   auto it = acnts.find( symbol.code().raw() );
+   eosio_assert( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+   eosio_assert( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+   acnts.erase( it );
+}
+
+} /// namespace eosio
+
+EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire) )
