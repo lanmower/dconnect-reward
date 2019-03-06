9f58a95ee4b1f46216320c28a2a3cb71bdd582aa
dev <almagestfraternite@gmail.com>
2019-03-06 19:28:03 +0000

    1.0

--- dconnect-reward.cpp
+++ dconnect-reward.cpp
@@ -28,7 +28,7 @@ void token::create( name   issuer,
     statstable.emplace( _self, [&]( auto& s ) {
        s.bounty_contract = bounty_contract;
        s.bounty   	 = bounty;
-       s.bounty_start    = now(); //we record the starting point
+       s.lastpay         = now(); //we record the starting point
        s.bounty_rate     = bounty_rate;
        s.supply.symbol   = maximum_supply.symbol;
        s.max_supply      = maximum_supply;
@@ -69,6 +69,7 @@ void token::issue( name to, asset quantity, string memo )
 }
 void token::retire( name to, asset quantity, string memo )
 {
+    require_auth( to );
     auto sym = quantity.symbol;
     eosio_assert( sym.is_valid(), "invalid symbol name" );
     eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
@@ -78,35 +79,54 @@ void token::retire( name to, asset quantity, string memo )
     eosio_assert( existing != statstable.end(), "token with symbol does not exist" );
     const auto& st = *existing;
 
-    require_auth( st.issuer );
     eosio_assert( quantity.is_valid(), "invalid quantity" );
     eosio_assert( quantity.amount > 0, "must retire positive quantity" );
 
     eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
 
+    float_t quantity_amount  = (float_t)(quantity.amount);
+    float_t supply_amount = (float_t)(st.supply.amount);
+    float_t share = (float_t)(quantity_amount / supply_amount);
+    uint64_t amount = st.bounty.amount*share;
+    asset payout_asset = asset((uint64_t)4, st.bounty.symbol);
+    payout_asset.amount = amount/(st.bounty_rate/86400);
+
+    print(" quantity_amount: ");
+    print(quantity.amount);
+    print(" supply_amount: ");
+    print(st.supply.amount);
+    print(" share: ");
+    print(share);
+    print(" amount: ");
+    print(payout_asset.amount);
+    eosio_assert(payout_asset.amount>0, "Not enough to claim with.");
+    sub_balance( to, quantity );
     statstable.modify( st, same_payer, [&]( auto& s ) {
-       s.supply -= quantity;
+     s.supply -= quantity;
     });
+    payouts payoutstable( _self, _self.value );
+    payoutstable.emplace( _self, [&]( auto& a ){
+      a.pk = payoutstable.available_primary_key();
+      a.bounty = payout_asset;
+      a.to = to;
+      a.memo = memo;
+      a.time = now();
+      a.quantity = quantity;
+    });
+}
 
-    sub_balance( st.issuer, quantity );
+void token::pay() {
+    require_auth( _self );
 
-    accounts from_acnts( st.bounty_contract, to.value );
-    const auto& from = from_acnts.get( st.bounty.symbol.raw(), "no balance object found" );
-     
-    int64_t bounty = st.bounty.amount;
-    float_t period = (now() - st.bounty_start) / st.bounty_rate;
-    float_t share = (float_t) quantity.amount / (float_t) st.supply.amount;
-    int64_t amount = (int64_t)(share/period);
-    asset payout = asset((int64_t)4, st.bounty.symbol);
-    payout.amount = amount;
-    
-    action(permission_level{ _self, name("active") },
-           name("eosio.token"), name("transfer"),
-           std::make_tuple( _self, to, payout, memo)
-    ).send();
-    //SEND_INLINE_ACTION( *this, transfer, { { _self, "active"_n} },
-    //                    { st.issuer, , quantity, memo }
-    //);
+    payouts payoutstable( _self, _self.value );
+
+    for(auto itr = payoutstable.begin(); itr != payoutstable.end();) {
+      action(permission_level{ _self, name("active") },
+       name("eosio.token"), name("transfer"),
+       std::make_tuple( _self, itr->to, itr->bounty, itr->memo)
+      ).send();
+      itr = payoutstable.erase(itr);
+    }
 }
 
 void token::transfer( name    from,
@@ -192,4 +212,4 @@ void token::close( name owner, const symbol& symbol )
 
 } /// namespace eosio
 
-EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire) )
+EOSIO_DISPATCH( eosio::token, (create)(issue)(transfer)(open)(close)(retire)(pay) )
